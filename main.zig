9d5fd3340342a4f0cd47ef7629f81cf98dc41369
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2025-05-12 06:40:57 +1000

    still crashes

--- main.zig
+++ main.zig
@@ -6,10 +6,10 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     var server = try httpz.Server(void).init(allocator, .{ .port = 3000 }, {});
     var router = try server.router(.{});
-    router.get("/", makeHandler("home", home), .{});
+    router.get("/", makeHandler(home), .{});
 }
 
-fn makeHandler(_: []const u8, func: fn (comptime []const u8, anytype) anyerror!void) fn (*httpz.Request, *httpz.Response) anyerror!void {
+fn makeHandler(func: fn (comptime []const u8, anytype) anyerror!void) fn (*httpz.Request, *httpz.Response) anyerror!void {
     return struct {
         fn handler(_: *httpz.Request, _: *httpz.Response) !void {
             try func("", "");
